c00e6d618ae62937428d9c73496dddc60c8c06a8
himayrobr <169061000+himayrobr@users.noreply.github.com>
2024-06-14 07:33:42 -0500

    feat: :sparkles: Galeria de imagenes de detalles

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
-import{galleryIndex} from "./components/gallery.js";
-import{getAllProductName} from "./module/app.js";
+import { menuListCategoryIndex } from "./components/menu.js";
+import { galleryIndex } from "./components/gallery.js";
+import { getAllProductName, getAllCategory } from "./module/app.js";
+
 
 let input__search = document.querySelector("#input__search");
 let main__article = document.querySelector(".main__article");
-let nav__ul = document.querySelector("nav__ul");
+let nav__ul = document.querySelector(".nav__ul");
 
 addEventListener("DOMContentLoaded", async e=>{
-    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSONstringify(await getAllCategory()));
-    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));
+    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
+    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));  
 })
 
-input__search.addEventListener("change", async e =>{
+
+input__search.addEventListener("change", async e => {
     let params = new URLSearchParams(location.search);
-    let data = {search : e.target.value, id: params.get('id')}
+    let data = { search : e.target.value, id: params.get('id')}
     input__search.value = null;
     let res = await getAllProductName(data)
-    main__article.innerHTML = galleryIndex (res, params.get('id'));
+    main__article.innerHTML = galleryIndex(res, params.get('id'));
 });
